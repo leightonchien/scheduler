eac8577fea5b0f8f0493b7173c66328066ec889e
Leighton Chien <leightonchien@gmail.com>
2021-04-12 22:18:55 -0700

    add unit test (DayListItem.test.js) + create function formatSpots in (DayListItem.js)

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,8 +2,22 @@ import React from "react";
 import "components/DayListItem.scss";
 const classNames = require('classnames');
 
-// props: name, selected, spots, set day
+
+const formatSpots = spots => {
+    if (!spots) {
+      return `no spots remaining`;
+    }
+  
+    if (spots === 1) {
+      return `${spots} spot remaining`;
+    }
+  
+    return `${spots} spots remaining`;
+  }
+
 export default function DayListItem(props) {
+    const availabilityMessage = formatSpots(props.spots);
+
     let dayClass = classNames('day-list__item', {
         'day-list__item--selected': props.selected,
         'day-list__item--full': !props.spots
@@ -16,7 +30,7 @@ export default function DayListItem(props) {
     >
 
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{availabilityMessage}</h3>
     </li>
   );
 }
