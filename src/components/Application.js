ff130bb73215e54ad3c593342d8f289772f31ad8
Leighton Chien <leightonchien@gmail.com>
2021-04-12 22:28:22 -0700

    integrate DayList component into app + import useState Hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 
+import DayList from "./DayList";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
 export default function Application(props) {
+
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
 
-  {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       <img
   className="sidebar--centered"
   src="images/logo.png"
@@ -15,6 +37,13 @@ export default function Application(props) {
 />
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu"></nav>
+<nav className="sidebar__menu">
+        <DayList
+          days={days}
+          day={day}
+          setDay={setDay}
+        />
+        </nav>
 <img
   className="sidebar__lhl sidebar--centered"
   src="images/lhl.png"
